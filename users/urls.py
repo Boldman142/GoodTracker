411783d6358df984ef9ff2c6c0b4263d8a921138
Artem Blodov <mr.samy@rambler.ru>
2024-04-06 12:59:33 +0200

    done

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+
 from rest_framework import routers
 
 from users.views import UserViewSet
@@ -8,9 +8,6 @@ from users.apps import UsersConfig
 app_name = UsersConfig.name
 
 urlpatterns = [
-    # path('logout/', LogoutView.as_view(), name='logout'),
-    # path('register/', RegisterView.as_view(), name='register'),
-    # path('profile/', UserUpdateView.as_view(), name='profile')
 ]
 router = routers.SimpleRouter()
 router.register(r'user', UserViewSet)
